316b5da0cf6bfd78a38bedd09080a2384e6d3f82
Stéphanie Océane FAULA <oceane.faula77@hotmail.com>
2022-05-02 16:17:02 +0200

    modification de la db

--- backend/doc/tables_sql/user.sql
+++ backend/doc/tables_sql/user.sql
@@ -6,6 +6,7 @@ CREATE TABLE user (
     id SERIAL PRIMARY KEY,
     firstname VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
+    pwd VARCHAR(255) NOT NULL,
     isAdmin TINYINT(1) NOT NULL DEFAULT(0),
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP
